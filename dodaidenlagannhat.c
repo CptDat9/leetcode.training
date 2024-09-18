0209f9df59bf9a845f0961534b033afdbfeea72a
Doan Dat <144463212+CptDat9@users.noreply.github.com>
2024-09-18 22:28:40 +0700

    Update dodaidenlagannhat.c

--- dodaidenlagannhat.c
+++ dodaidenlagannhat.c
@@ -23,3 +23,23 @@ int minDepth(struct TreeNode* root) {
         return ((minDepth(root->left))<(minDepth(root->right)))?(1+minDepth(root->left)) :(1+minDepth(root->right));
     }
 }
+/// Cải tiến tránh stack overflow
+int minDepth(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (root->left == NULL) {
+        return minDepth(root->right) + 1;
+    }
+
+    if (root->right == NULL) {
+        return minDepth(root->left) + 1;
+    }
+
+    // Nếu cả hai cây con đều không phải NULL, tính độ sâu nhỏ nhất giữa hai cây con
+    int leftDepth = minDepth(root->left);
+    int rightDepth = minDepth(root->right);
+
+    return (leftDepth < rightDepth ? leftDepth : rightDepth) + 1;
+}
